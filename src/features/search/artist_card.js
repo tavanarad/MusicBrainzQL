4b9a412f6d8614b7001c3895700a389ef2223382
Morteza Tavanarad <tavanarad@gmail.com>
2021-07-02 03:21:14 +0800

    Added onClick property

--- src/features/search/artist_card.js
+++ src/features/search/artist_card.js
@@ -4,6 +4,7 @@ import {
   CardMedia,
   makeStyles,
   Typography,
+  CardActionArea,
 } from "@material-ui/core";
 import { Rating } from "@material-ui/lab";
 import PropTypes from "prop-types";
@@ -38,48 +39,52 @@ const useStyles = makeStyles(() => ({
   },
 }));
 
-function ArtistCard({ artist }) {
+function ArtistCard({ artist, onClick }) {
   const classes = useStyles();
+  const handleOnClick = () => onClick(artist.node.mbid);
+
   return (
     <Card className={classes.artistCard}>
-      <CardMedia className={classes.albumImages} title={artist.node.name}>
-        <img
-          alt={artist.node.name}
-          src={
-            artist.node.mediaWikiImages.length
-              ? artist.node.mediaWikiImages[0].url
-              : artistIcon
-          }
-          className={classes.artistImage}
-        />
-      </CardMedia>
-      <CardContent>
-        <Typography variant="h6" gutterBottom>
-          {artist.node.name}
-        </Typography>
-        <Typography
-          className={classes.caption}
-          variant="caption"
-          display="inline-block"
-          gutterBottom
-        >
-          {artist.node.disambiguation}
-        </Typography>
-        <Typography
-          className={classes.rating}
-          variant="caption"
-          display="inline"
-          gutterBottom
-        >
-          ({artist.node.rating.voteCount || 0})
-        </Typography>
-        <Rating
-          className={classes.rating}
-          size="small"
-          value={artist.node.rating?.value}
-          readOnly
-        />
-      </CardContent>
+      <CardActionArea onClick={handleOnClick}>
+        <CardMedia className={classes.albumImages} title={artist.node.name}>
+          <img
+            alt={artist.node.name}
+            src={
+              artist.node.mediaWikiImages.length
+                ? artist.node.mediaWikiImages[0].url
+                : artistIcon
+            }
+            className={classes.artistImage}
+          />
+        </CardMedia>
+        <CardContent>
+          <Typography variant="h6" gutterBottom>
+            {artist.node.name}
+          </Typography>
+          <Typography
+            className={classes.caption}
+            variant="caption"
+            display="inline-block"
+            gutterBottom
+          >
+            {artist.node.disambiguation}
+          </Typography>
+          <Typography
+            className={classes.rating}
+            variant="caption"
+            display="inline"
+            gutterBottom
+          >
+            ({artist.node.rating.voteCount || 0})
+          </Typography>
+          <Rating
+            className={classes.rating}
+            size="small"
+            value={artist.node.rating?.value}
+            readOnly
+          />
+        </CardContent>
+      </CardActionArea>
     </Card>
   );
 }
