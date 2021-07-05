46df605c5d740f741393adaa70acab876fe69a38
Morteza Tavanarad <tavanarad@gmail.com>
2021-07-05 10:45:24 +0800

    Set the default GraphBrainz URL to localhost:8080

--- src/configs.js
+++ src/configs.js
@@ -1,2 +1,2 @@
-export const MUSIC_BRAINS_SERVER = "http://api.ql.tavanarad.ir/";
+export const MUSIC_BRAINS_SERVER = "http://localhost:8080";
 export const FETCH_CORS_MODE = "no-cors" // possible values: cors, no-cors, or same-origin
